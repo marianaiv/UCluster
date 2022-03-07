ff6a064aa16723dc438ac137d70fc822e1b84952
marianaiv <marianaalbrnz@gmail.com>
2022-03-06 21:30:13 -0400

    trying concat again

--- scripts/concat_masterkey.py
+++ scripts/concat_masterkey.py
@@ -30,32 +30,10 @@ N_EVENTS = flags.datasize
 # Reading the label
 df_key = ascii_column(os.path.join(SAMPLES_PATH,key))
 
-f = h5py.File(os.path.join(SAVE_PATH,'BlackBox{}_with_key.h5'.format(BB)), 'a')
+list_df = []
 
-# doing it with a loop
-for ii in range(int(N_EVENTS/CHUNKSIZE)):
-    print(start)
-    # Reading the files for the data
-    df_bb = pd.read_hdf(os.path.join(SAMPLES_PATH,sample), start=start, stop=start+CHUNKSIZE)
-    # Joining the df's
-    bb_with_key = df_bb.assign(label=pd.Series(df_key.iloc[start:start+CHUNKSIZE,0]).values)
-    bb_with_key.rename(columns={'label': 2100}, inplace=True)
-    
-    bb_array = bb_with_key.to_numpy()
 
-    if ii == 0:
-        f.create_dataset('bb', data=bb_array, compression="gzip", chunks=True, maxshape=(None, 2101))
-        print(f['bb'].shape)
-    else:
-        # Append new data to it
-        print(f['bb'].shape[0] , bb_array.shape[0])
-        f['bb'].resize((f['bb'].shape[0] + bb_array.shape[0]), axis=0)
-        f['bb'][-bb_array.shape[0]:] = bb_array
-        
-    start+=CHUNKSIZE 
-
-'''
-
+# looping to concat with key
 while start != N_EVENTS:
     print(start)
     # Reading the files for the data
@@ -63,15 +41,21 @@ while start != N_EVENTS:
     # Joining the df's
     bb_with_key = df_bb.assign(label=pd.Series(df_key.iloc[start:start+CHUNKSIZE,0]).values)
     bb_with_key.rename(columns={'label': 2100}, inplace=True)
-
+    
     list_df.append(bb_with_key)
 
-'''
-print(f['bb'].shape)
+
+# looping to concat with each other
+for ii in range(len(list_df)-1):
+    if ii == 0:
+        final_df = pd.concat(list_df[:1], ignore_index=True)
+
+    else:
+        final_df = pd.concat([final_df, list_df[ii+1]], ignore_index=True)
     
 
 # Saving as h5
-#df_final = pd.concat(list_df, ignore_index=True)
-#df_final.to_hdf(os.path.join(SAVE_PATH,'BlackBox{}_with_key.h5'.format(bb)), key='bb')
+#final_df = pd.concat(list_df, ignore_index=True)
+final_df.to_hdf(os.path.join(SAVE_PATH,'BlackBox{}_with_key.h5'.format(bb)), key='bb')
 
 print('Succesfully joined')
